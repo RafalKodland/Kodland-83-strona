eebd5d4036b8eb8a3c16d3f58c3c4ce8aca83077
Rafał Rusin <rafal.rusin@o365.us.edu.pl>
2024-11-13 18:59:25 +0100

    M3L1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("test")
-print("druga linia")
+from flask import Flask
+from random import randint
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Witaj Świecie!</h1><a href='/test'>Przejdź do strony testowej</a>"
+
+@app.route("/test")
+def test():
+    return f"<p>Strona testowa. Losowa liczba: {randint(1, 100)}</p><a href='/'>Cofnij się</a>"
+
+app.run(debug=True)
